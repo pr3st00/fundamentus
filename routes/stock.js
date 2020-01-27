f3aa3f7582f7bb5d1536b13e817955d98fcc5169
Fernando Almeida <fernando.c.almeida@gmail.com>
2020-01-27 10:13:12 -0200

    initial commit

---
+++ routes/stock.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const rp = require('request-promise');
+const $ = require('cheerio');
+const baseUrl = 'http://www.fundamentus.com.br/detalhes.php?papel=';
+
+router.get('/', function(req, res, next) {
+
+  const ticker = req.query.ticker;
+  const url = baseUrl + ticker;
+
+  rp(url)
+  .then(function(html){
+
+    let spans = [];
+
+    $('span', html).each(function (i, e) {
+      spans[i] = $(this).text().replace(/\s/g,'');
+    });
+
+    console.debug(spans);
+
+    res.send({ ticker: ticker,
+      dy: spans[104],
+      roe: spans[107],
+      roic: spans[99],
+      eve: spans[112],
+      pl: spans[48],
+      pvp: spans[56],
+    });
+
+  })
+  .catch(function(err){
+    //handle error
+  });
+
+});
+
+module.exports = router;
