632d4f6af68e9998128fdcf43938ec5d7150c6aa
Fernando Almeida <fernando.c.almeida@gmail.com>
2020-01-27 22:04:13 -0200

    updates

--- routes/fii.js
+++ routes/fii.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const rp = require('request-promise');
+const $ = require('cheerio');
+const baseUrl = 'https://www.fundsexplorer.com.br/funds/';
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+
+  const ticker = req.query.ticker;
+  const url = baseUrl + ticker;
+
+  rp(url)
+    .then(function (html) {
+
+      let spans = [];
+
+      $('span', html).each(function (i, e) {
+        spans[i] = $(this).text().replace(/\s|%|-/g, '').replace(/,/g, '.');
+      });
+
+      console.debug(spans);
+
+      res.send({
+        ticker: ticker,
+        pvp: spans[41],
+        dy: spans[33],
+      });
+
+    })
+    .catch(function (err) {
+      res.status(500)
+      res.send({ error: err })
+    });
+
 });
 
 module.exports = router;
--- routes/stock.js
+++ routes/stock.js
@@ -4,35 +4,37 @@ const rp = require('request-promise');
 const $ = require('cheerio');
 const baseUrl = 'http://www.fundamentus.com.br/detalhes.php?papel=';
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
   const ticker = req.query.ticker;
   const url = baseUrl + ticker;
 
   rp(url)
-  .then(function(html){
+    .then(function (html) {
 
-    let spans = [];
+      let spans = [];
 
-    $('span', html).each(function (i, e) {
-      spans[i] = $(this).text().replace(/\s|%|-/g,'').replace(/,/g,'.');
+      $('span', html).each(function (i, e) {
+        spans[i] = $(this).text().replace(/\s|%|-/g, '').replace(/,/g, '.');
+      });
+
+      console.debug(spans);
+
+      res.send({
+        ticker: ticker,
+        dy: spans[104],
+        roe: spans[107],
+        roic: spans[99],
+        eve: spans[112],
+        pl: spans[48],
+        pvp: spans[56],
+      });
+
+    })
+    .catch(function (err) {
+      res.status(500)
+      res.send({ error: err })
     });
-
-    console.debug(spans);
-
-    res.send({ ticker: ticker,
-      dy: spans[104],
-      roe: spans[107],
-      roic: spans[99],
-      eve: spans[112],
-      pl: spans[48],
-      pvp: spans[56],
-    });
-
-  })
-  .catch(function(err){
-    //handle error
-  });
 
 });
 
