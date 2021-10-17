0620ecbabd0c6d8d2d4fa54c0b066b0e7dbc1d60
Fernando Almeida <fernando.c.almeida@gmail.com>
2021-10-17 15:16:37 -0300

    Rest like urls

--- routes/fii.js
+++ routes/fii.js
@@ -5,25 +5,31 @@ const crawler = require('../lib/crawler.js');
 const baseUrl = 'https://www.fundsexplorer.com.br/funds/';
 
 router.get('/', function (req, res, next) {
+  sendResponse(req.query.ticker, res);
+});
 
-  const ticker = req.query.ticker;
+router.get('/:ticker', function (req, res, next) {
+  sendResponse(req.params.ticker, res);
+});
+
+function sendResponse(ticker, res) {
   const url = baseUrl + ticker;
 
   crawler(ticker, url, (ticker, spans) => {
-	  return {
-        ticker: ticker,
-        pvp: spans[spans.findIndex(e => e =="P/VP") + 1],
-        dy: spans[spans.findIndex(e => e =="DividendYield") + 1],
-      };
+    return {
+      ticker: ticker,
+      pvp: spans[spans.findIndex(e => e == "P/VP") + 1],
+      dy: spans[spans.findIndex(e => e == "DividendYield") + 1],
+    };
   })
-  .then(function (returnValue) {
-		res.send(returnValue);
-  })
-  .catch(function (err) {
-		res.status(500);
-		res.send({ error: err })
-  });
- 
-});
+    .then(function (returnValue) {
+      res.send(returnValue);
+    })
+    .catch(function (err) {
+      res.status(500);
+      res.send({ error: err })
+    });
+
+}
 
 module.exports = router;
--- routes/stock.js
+++ routes/stock.js
@@ -5,32 +5,37 @@ const crawler = require('../lib/crawler.js');
 const baseUrl = 'http://www.fundamentus.com.br/detalhes.php?papel=';
 
 router.get('/', function (req, res, next) {
+  sendResponse(req.query.ticker, res);
+});
 
-  const ticker = req.query.ticker;
+router.get('/:ticker', function (req, res, next) {
+  sendResponse(req.params.ticker, res);
+});
+
+function sendResponse(ticker, res) {
   const url = baseUrl + ticker;
   const cotacaoRegex = /Cota.*o/g;
 
   crawler(ticker, url, (ticker, spans) => {
-	  return {
-        ticker: ticker,
-        dy: spans[spans.findIndex(e => e =="Div.Yield") + 1],
-        roe: spans[spans.findIndex(e => e =="ROE") + 1],
-        roic: spans[spans.findIndex(e => e =="ROIC") + 1],
-        eve: spans[spans.findIndex(e => e =="EV/EBITDA") + 1],
-        pl: spans[spans.findIndex(e => e =="P/L") + 1],
-        pvp: spans[spans.findIndex(e => e =="P/VP") + 1],
-        lpa: spans[spans.findIndex(e => e =="LPA") + 1],
-        price: spans[spans.findIndex(e => e.match(cotacaoRegex)) + 1],
-      };
+    return {
+      ticker: ticker,
+      dy: spans[spans.findIndex(e => e == "Div.Yield") + 1],
+      roe: spans[spans.findIndex(e => e == "ROE") + 1],
+      roic: spans[spans.findIndex(e => e == "ROIC") + 1],
+      eve: spans[spans.findIndex(e => e == "EV/EBITDA") + 1],
+      pl: spans[spans.findIndex(e => e == "P/L") + 1],
+      pvp: spans[spans.findIndex(e => e == "P/VP") + 1],
+      lpa: spans[spans.findIndex(e => e == "LPA") + 1],
+      price: spans[spans.findIndex(e => e.match(cotacaoRegex)) + 1],
+    };
   })
-  .then(function (returnValue) {
-		res.send(returnValue);
-  })
-  .catch(function (err) {
-		res.status(500);
-		res.send({ error: err })
-  });
- 
-});
+    .then(function (returnValue) {
+      res.send(returnValue);
+    })
+    .catch(function (err) {
+      res.status(500);
+      res.send({ error: err })
+    });
+}
 
 module.exports = router;
