28e4d5eff20e34ddbe32ee8ff9dcf84543c901c9
Fernando Almeida <fernando.c.almeida@gmail.com>
2023-06-05 18:13:40 -0300

    Fii fix

--- routes/fii.js
+++ routes/fii.js
@@ -4,7 +4,12 @@ const cheerio = require('cheerio');
 const crawler = require('../lib/crawler.js');
 
 const baseUrl = 'https://www.fundsexplorer.com.br/funds/';
-const NA = "N/A";
+
+const NA  = "N/A";
+const PVP = "P/VP";
+const SEGMENT = "Segmento";
+const DY = /DY.*Dividendo/;
+const PRICE = /Cota.*atual/;
 
 router.get('/', function (req, res, next) {
   sendResponse(req.query.ticker, res);
@@ -25,22 +30,22 @@ function sendResponse(ticker, res) {
   crawler(ticker, url, (ticker, html) => {
     let $ = cheerio.load(html);
 
-    let price = $("#stock-price span").first().text().replace(/,/g, '.').replace(/ /g, '').replace(/R\$/g, '').replace(/\n/g, '');
+    let price = $("#headerTicker__content__price div").first().text().replace(/,/g, '.').replace(/ /g, '').replace(/R\$/g, '').replace(/\n/g, '');
     let properties = $("#fund-actives-chart-info-wrapper span").first().text().replace(/ativos/g,'').replace(/ /g,'');
 
     let p = [];
 
     $('p').each(function (i, e) {
-      p[i] = $(this).text().replace(/%|-/g, '').replace(/,/g, '.');
+      p[i] = $(this).text();
     });
 
     return {
       ticker: ticker,
-      pvp: p[p.findIndex(e => e == "P/VP") + 1].replace(/\s/g, ''),
-      dy: p[p.findIndex(e => e.match(/DY.*Dividendo/)) + 1].replace(/\s/g, ''),
-      sector: p[p.findIndex(e => e == "Segmento") + 1],
-      properties: properties || NA,
-      price: price,
+      pvp: 		formatNumber(p[p.findIndex(e => e == PVP) + 1]),
+      dy:  		formatNumber(p[p.findIndex(e => e.match(DY)) + 1]),
+      price: 		formatNumber(p[p.findIndex(e => e.match(PRICE)) - 1]),
+      sector: 		formatText(p[p.findIndex(e => e == SEGMENT) + 1]),
+      properties: 	properties || NA,
     };
   })
     .then(function (returnValue) {
@@ -59,6 +64,14 @@ function sendResponse(ticker, res) {
       });
     }, options);
 
+}
+
+function formatText(value) {
+	return value;
+}
+
+function formatNumber(value) {
+	return value.replace(/\s|%|-/g, '').replace(/,/g, '.').replace(/R\$/g, '');
 }
 
 module.exports = router;
