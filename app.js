e5b04ce2a8fcee6cb05c7f360ec6acfb8f028bcd
Fernando Almeida <fernando.x.almeida@t-mobile.com>
2026-02-28 02:11:20 -0300

    JSON logging

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
-const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const loggerBuilder = require('./lib/loggerBuilder');
 
+// Routes
 const fundsexplorer = require('./routes/fii/fundsexplorer');
 const investidor10 = require('./routes/fii/investidor10');
 const stock = require('./routes/stocks/fundamentus');
-
 const health = require('./routes/health');
 
 const app = express();
@@ -17,9 +17,9 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(express.json());
+app.use(loggerBuilder.createLogger());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
